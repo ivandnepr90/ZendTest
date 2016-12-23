2ec9f98149badcb8dbe46e97301d905109144ad3
Ivan <Vanyk90.22@gmail.com>
2016-12-23 04:12:52 +0200

    Add module album

---
+++ module/Album/config/module.config.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ivan
+ * Date: 26.11.2016
+ * Time: 17:44
+ */
+
+namespace Album;
+
+use Album\Controller\AlbumController;
+use Zend\Router\Http\Segment;
+
+
+return [
+
+
+    // The following section is new and should be added to your file:
+    'router' => [
+        'routes' => [
+            'album' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route' => '/album[/:action[/:id]]',
+                    'constraints' => [
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ],
+                    'defaults' => [
+                        'controller' => Controller\AlbumController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+
+
+    'view_manager' => [
+        'template_path_stack' => [
+            'album' => __DIR__ . '/../view',
+        ],
+    ],
+];
